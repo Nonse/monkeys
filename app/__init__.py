93d722f9554169b4f39ad905dae579eb8c0b33df
Maria Klepikova <purr.effect@gmail.com>
2015-01-12 00:17:37 +0200

    Added Bootstrap & base.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+Bootstrap(app)
 
-from app import models
+
+from app import models, views
